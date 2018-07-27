8d6dd93c2b0bc3fefeba7065971f6d5dd02812c9
Bram van Heuvelen <bramvanheuvelen@gmail.com>
2018-07-27 15:56:01 +0200

    jaja

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react'
 
 
+
+
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { term: '' };
+    }
+
     render() {
-        return <input />
+        return <input onChange={event => console.log(event.target.value)} />
     }
 }
 
