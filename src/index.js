15abef90155db8daa630aea0bf1b7f8ff026bf87
Bram van Heuvelen <bramvanheuvelen@gmail.com>
2018-07-27 13:18:42 +0200

    jaja

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
+import React from 'react';
+import ReactDom from 'react-dom'
+
 // Create a new component. This component should produce
 // some HTML
-const App = function() {
+const App = () => {
     return <div>Hi!</div>
 }
 
-
-
 //take this components generated html, and put it
 // on the page (in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'))
