d3406126a2095fa70cf5c77ccd041770f02e2f18
Bram van Heuvelen <bramvanheuvelen@gmail.com>
2018-07-27 12:53:51 +0200

    index

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// Create a new component. This component should produce
+// some HTML
+const App = function() {
+    return <div>Hi!</div>
+}
+
+
+
+//take this components generated html, and put it
+// on the page (in the DOM)
